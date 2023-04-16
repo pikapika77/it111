86f179d1e39a78d77eff9af02f6f86fae801d365
pikapika77 <129773613+pikapika77@users.noreply.github.com>
2023-04-16 16:53:18 -0700

    Add files via upload

---
+++ week1/MyDataTypes.java
@@ -0,0 +1,29 @@
+public class MyDataTypes {
+
+    public static void main(String[] args) {
+        short number1 = 32_000;
+        int number2 = 2_000_000_000;
+        long number3 = 3_000_000_000L;
+        float number4 = 30.57F;
+        double number5 = 50.4577654;
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5);
+
+        boolean dry = false;
+
+        if (dry) {
+
+            System.out.println("Time to hang up those curtains!");
+        }
+        else {
+                System.out.println("We will have to wait a spell!");
+            }
+        char best = 'A';
+        System.out.println(++best);
+        }
+    }
+
---
+++ week1/MyVariable1.java
@@ -0,0 +1,11 @@
+public class MyVariable1 {
+
+    public static void main(String[]args) {
+
+String name = "Tori";
+        int hours = 40;
+
+        System.out.println(name);
+        System.out.println(hours);
+    }
+}
